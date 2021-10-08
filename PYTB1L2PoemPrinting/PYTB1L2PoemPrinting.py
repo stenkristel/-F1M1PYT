415beffc7b91caedd42dc142386ed1c9920e7ddb
stenkristel <stenkristel2016@gmail.com>
2021-10-08 10:09:38 +0200

    Typfout verbeterd

---
+++ PYTB1L2PoemPrinting/PYTB1L2PoemPrinting.py
@@ -0,0 +1,9 @@
+
+vers1 = "Hallo, mijn naam is Sten,"
+vers2 = "Ik breek vaak mijn Pen."
+vers3 = "Ik eet graag fruit saladen,"
+vers4 = "Maar niet met tomaten"
+vers5 = "Ik drink meestal water,"
+vers6 = "Sten out, ik zie later."
+
+print (vers1, vers2, vers3, vers4, vers5, vers6)

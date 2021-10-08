feea5e5e64f6edb3692964cc83a17be3a3586e5a
stenkristel <stenkristel2016@gmail.com>
2021-10-08 10:17:58 +0200

    Typefout verbeterd

--- PYTB1L2PoemPrinting/PYTB1L2PoemPrinting.py
+++ PYTB1L2PoemPrinting/PYTB1L2PoemPrinting.py
@@ -4,6 +4,6 @@ vers2 = "Ik breek vaak mijn Pen."
 vers3 = "Ik eet graag fruit saladen,"
 vers4 = "Maar niet met tomaten"
 vers5 = "Ik drink meestal water,"
-vers6 = "Sten out, ik zie later."
+vers6 = "Sten out, ik zie je later."
 
 print (vers1, vers2, vers3, vers4, vers5, vers6)

29075771e10dcc49b6c213ee498a9f3787e5f200
stenkristel <stenkristel2016@gmail.com>
2021-10-12 12:37:34 +0200

    Player stats gemaakt

---
+++ PYTB1L2PlayerStatistics/PYTB1L2PlayerStatistics.py
@@ -0,0 +1,21 @@
+Str = 8
+Con = 14
+Dex = 16
+Int = 10
+Wis = 11
+Cha = 18
+Exp = "3450/5000"
+Suc = "●○○"
+Ins = "True"
+Class = "Scorcerer (Dragonic Bloodline)"
+
+print("Strenght:", Str)
+print("Constitution:", Con)
+print("Dexterity:", Dex)
+print("Inteligence:", Int)
+print("Wisdom:", Wis)
+print("Charisma:", Cha)
+print("Experience:", Exp)
+print("Death saves successes:", Suc)
+print("Inspiration:", Ins)
+print("Class:", Class)
